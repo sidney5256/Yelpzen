ea0214170c4e81817f335d6c6dba9df1f2ed3d90
sidney5256 <sep394@nyu.edu>
2018-11-12 09:52:50 -0500

    Changed how we sort through venues for specific category or 'popularity'

---
+++ src/categories.js
@@ -0,0 +1,8 @@
+const categories = {
+	Food: ['Restaurant', 'Food & Beverages', 'Bars & Pubs', 'Deli', 'Coffee & Tea', 'Bakery', 'Fast Food', 'Food Court', 'Cafe', 'Ice Cream', 'Donuts', 'Juice Bar'],
+	Shopping: ['Shopping', 'Retail', 'Shopping Center', 'Wholesale'],
+	Entertainment: ['Arts & Performance', 'Cinema', 'Recreation', 'Museum', 'Casinos & Gambling', 'Brewery', 'Sports & Exercise'],
+	Services: ['Travel', 'Health Services', 'Real Estate', 'Banks & Credit Unions', 'Social Services', 'Autos & Motor Vehicles', 'Utilities', 'Taxi', 'Building & Trades']
+}
+
+export default categories 
